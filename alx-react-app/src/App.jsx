e01f882060b1cb888d3ca6a409ad23c79607c0d4
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-07-11 00:24:35 +0300

    2. Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import MainContent from "./components/MainContent"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +33,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    )
+}
