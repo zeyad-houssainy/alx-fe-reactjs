4992c495842ff14fa4b5e8490b815e1bc7d03c71
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-07-19 01:14:25 +0300

    1. Create a Simple Counter Application Using State and Hooks

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -3,17 +3,17 @@ import React, { useState } from "react"
 export default function Counter() {
     const [count, setCount] = useState(0)
 
-    const increment = () => {setCount(count + 1 )}
-    const decrease = () => {setCount(count - 1 )}
-    const reset = () => {setCount(0)}
+    const Increment = () => {setCount(count + 1 )}
+    const Decrement = () => {setCount(count - 1 )}
+    const Reset = () => {setCount(0)}
 
     return(
         <div className="visit-times" style={{border:"1px solid yellow"}}>
             <h3>Number of Visits</h3>
             <p>I've been visited {count} Times</p>
-            <button onClick={increment}>Increase</button>
-            <button onClick={reset}>reset</button>
-            <button onClick={decrease}>decrease</button>
+            <button onClick={Increment}>Increment</button>
+            <button onClick={Reset}>Reset</button>
+            <button onClick={Decrement}>Decrement</button>
         </div>
     )
 }
