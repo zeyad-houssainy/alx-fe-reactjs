56b1e9533b562872d9c5a7f24b6af81365c18133
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-08-11 00:23:48 +0300

    1. Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 md:p-8 sm:p-4 sm:max-w-xs md:max-w-sm max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile max-w-sm sm:max-w-xs md:max-w-sm mx-auto my-20 sm:p-4 md:p-8 bg-gray-100 rounded-lg shadow-lg">
         
       <img src="https://via.placeholder.com/150" alt="User" 
-        className="rounded-full w-36 h-36 mx-auto"/>
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        className="w-36 h-36 mx-auto rounded-full"/>
+      <h1 className="my-4 text-blue-800 sm:text-lg md:text-xl">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
